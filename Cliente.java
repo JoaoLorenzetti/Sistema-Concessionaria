94ae8fafaf639a24b266ec142537baefcfe614f3
JoaoLorenzetti <lorenzettijp@gmail.com>
2025-10-16 20:22:39 -0300

    correção  classe Cliente

--- Cliente.java
+++ Cliente.java
@@ -15,16 +15,4 @@ public class Cliente extends Pessoa {
     public boolean isClientePremium() {
         return clientePremium;
     }
-
-    //Sobrescrita
-    @Override
-    public void exibirInformacoes() {
-        System.out.println("\n--- Informações do Cliente ---");
-        System.out.println("Nome: " + getNome());
-        System.out.println("CPF: " + getCpf());
-        System.out.println("Telefone: " + getTelefone());
-        System.out.println("Endereço: ");
-        getEndereco().exibirEndereco(); //Reflexividade
-        System.out.println("Cliente Premium: " + (clientePremium ? "Sim" : "Não"));
-    }
 }
